4e0c0f9f6bf0de5777d7f8c075215203f3c8cb3d
EnosPebrian <enospebrian1@gmail.com>
2023-10-05 10:19:23 +0700

    update api url

--- src/json-server/api.jsx
+++ src/json-server/api.jsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export const API_URL = "https://api-socmed.crystalux.web.id";
-// export const API_URL = "http://localhost:8001";
 
 export const api = axios.create({
   baseURL: API_URL,
